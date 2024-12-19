1da88ea0349a513e9faac11ebefe56925c632022
Nick <nick.lambourne@inovandum.com.au>
2024-12-19 19:50:20 +1100

    fix sql syntax

--- src/MHServerEmu.DatabaseAccess/Data/SQLite/InitializeLeaderboardsDatabase.sql
+++ src/MHServerEmu.DatabaseAccess/Data/SQLite/InitializeLeaderboardsDatabase.sql
@@ -8,7 +8,7 @@ CREATE TABLE "Leaderboards" (
 	"ActiveInstanceId"	INTEGER,
 	"IsActive"	INTEGER,
 	PRIMARY KEY("LeaderboardId")
-)
+);
 
 CREATE TABLE "Instances" (
 	"InstanceId"	INTEGER NOT NULL UNIQUE,
@@ -18,7 +18,7 @@ CREATE TABLE "Instances" (
 	"Visible"	INTEGER,
 	FOREIGN KEY("LeaderboardId") REFERENCES "Leaderboards"("LeaderboardId") ON DELETE CASCADE,
 	PRIMARY KEY("InstanceId" AUTOINCREMENT)
-)
+);
 
 CREATE TABLE "Entries" (
 	"Id"	INTEGER NOT NULL UNIQUE,
@@ -29,7 +29,7 @@ CREATE TABLE "Entries" (
 	"RuleStates"	BLOB,
 	FOREIGN KEY("InstanceId") REFERENCES "Instances"("InstanceId") ON DELETE CASCADE,
 	PRIMARY KEY("Id" AUTOINCREMENT)
-)
+);
 
 CREATE INDEX idx_instances_leaderboardid ON Instances (LeaderboardId);
 CREATE INDEX idx_entries_instanceid ON Entries (InstanceId);
